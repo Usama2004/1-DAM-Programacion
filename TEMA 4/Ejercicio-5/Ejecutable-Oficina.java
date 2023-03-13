2aafa1d0612eb71ca9ea59e76af585c9583b20fe
Oussama Ed Ed Dahabi Lakhal <114860895+Usama2004@users.noreply.github.com>
2023-03-13 19:08:57 +0100

    Add files via upload

---
+++ TEMA 4/Ejercicio-5/Ejecutable-Oficina.java
@@ -0,0 +1,18 @@
+package ejercicio5_oficina;
+
+/**
+ *
+ * @author Oussama <oedd308@g.educaand.es>
+ */
+public class Ejercicio5_Oficina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+       
+    }
+
+}
